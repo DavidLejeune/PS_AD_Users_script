8717625a9a9edea792f99f3c05c914677872ad76
David Lejeune <Lejeune.David@outlook.com>
2016-09-28 00:50:52 +0200

    Old PFSV1 AD PS tests

---
+++ PFSV1 tests/ADUsers.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module ActiveDirectory
+Get-ADUser -SearchBase "OU=PFAfdelingen,dc=POLIFORMADL,dc=COM" -Filter * -properties * -ResultSetSize 5000 | select CN ,SAMAccountName, Department, Description , Title,UserPrincipalName, DistinguishedName, HomeDirectory, ProfilePath, Office, OfficePhone, Manager    | convertto-html | out-file C:\Users\Administrator\Desktop\ADUsers.html
+Get-ADUser -SearchBase "OU=PFAfdelingen,dc=POLIFORMADL,dc=COM" -Filter * -properties * -ResultSetSize 5000 | select CN ,SAMAccountName, Department, Description , Title,UserPrincipalName, DistinguishedName, HomeDirectory, ProfilePath, Office, OfficePhone, Manager
+pause
---
+++ Scripts/1_4_one-liners.txt
@@ -0,0 +1,11 @@
+﻿break
+
+#List of all domain controllers
+Get-ADDomainController -Filter * | Format-Table Name, Domain, Forest, Site, IPv4Address, OperatingSystem
+
+#Account unlock
+Read-Host "Enter the user account to unlock" | Unlock-ADAccount
+
+#Password reset
+Set-ADAccountPassword (Read-Host 'User') -Reset
+
