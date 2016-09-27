47e12b3331d0cd2b32283e5a047b89aec41da1bf
David Lejeune <Lejeune.David@outlook.com>
2016-09-28 01:15:19 +0200

    Basic menu

---
+++ DaLe-DCdo.ps1
@@ -0,0 +1,26 @@
+﻿clear
+
+$Menu1 = "Create new OU";
+$Menu2 = "New User"
+
+Write-Host "Menu :";
+Write-Host "";
+Write-Host '    1. '$Menu1;
+Write-Host '    2. '$Menu2;
+Write-Host "";
+
+$Menu = Read-Host -Prompt 'Select an option '
+
+
+switch ($Menu) 
+    { 
+        1 
+        {
+            Write-Host 'You have selected'$Menu1;
+        } 
+        2 
+        {
+            Write-Host 'You have selected'$Menu2;
+        } 
+        default {"The choice could not be determined."}
+    }
