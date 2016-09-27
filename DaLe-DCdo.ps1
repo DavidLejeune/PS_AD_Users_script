15f380d4c3d4159d388d52d4a29a568e36d939ee
David Lejeune <Lejeune.David@outlook.com>
2016-09-28 01:38:10 +0200

    Create OU & create user

--- DaLe-DCdo.ps1
+++ DaLe-DCdo.ps1
@@ -1,4 +1,6 @@
-﻿clear
+﻿Import-Module ActiveDirectory
+
+clear
 
 $Menu1 = "Create new OU";
 $Menu2 = "New User"
@@ -17,10 +19,32 @@ switch ($Menu)
         1 
         {
             Write-Host 'You have selected'$Menu1;
+            Create-OU;
         } 
         2 
         {
             Write-Host 'You have selected'$Menu2;
+            Create-User;
         } 
         default {"The choice could not be determined."}
     }
+
+
+function Create-OU()
+{
+    $OUname = Read-Host -Prompt '> OU name ';
+    New-ADOrganizationalUnit $OUname;
+}
+
+
+
+function Create-User()
+{
+    $Username = Read-Host -Prompt '> full user ';
+    $Givenname = Read-Host -Prompt '> given name ';
+    $Surname = Read-Host -Prompt '> surname ';
+    $Displayname = Read-Host -Prompt '> display name ';
+    $UserpathOU = Read-Host -Prompt '> OU ';
+    New-ADUser -name $Username -GivenName $Givenname -SurName $Surname -DisplayName $Displayname -Path "ou="$UserpathOU",dc=POLIFORMADL.COM,dc=local";
+    
+}
