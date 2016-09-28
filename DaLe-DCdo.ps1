02f5446a1860e58234fc8776b7d21174f3baa651
David Lejeune <Lejeune.David@outlook.com>
2016-09-28 18:24:00 +0200

    DaLe script arranged scriptwise

--- DaLe-DCdo.ps1
+++ DaLe-DCdo.ps1
@@ -1,35 +1,7 @@
 ﻿Import-Module ActiveDirectory
 
-clear
-
 $Menu1 = "Create new OU";
 $Menu2 = "New User"
-
-Write-Host "Menu :";
-Write-Host "";
-Write-Host '    1. '$Menu1;
-Write-Host '    2. '$Menu2;
-Write-Host "";
-
-$Menu = Read-Host -Prompt 'Select an option ';
-
-
-
-switch ($Menu) 
-    { 
-        1 
-        {
-            Write-Host 'You have selected'$Menu1;
-            Create-OU;
-        } 
-        2 
-        {
-            Write-Host 'You have selected'$Menu2;
-            Create-User;
-        } 
-        default {"The choice could not be determined."}
-    }
-
 
 function Create-OU()
 {
@@ -46,6 +18,58 @@ function Create-User()
     $Surname = Read-Host -Prompt '> surname ';
     $Displayname = Read-Host -Prompt '> display name ';
     $UserpathOU = Read-Host -Prompt '> OU ';
-    New-ADUser -name $Username -GivenName $Givenname -SurName $Surname -DisplayName $Displayname -Path "ou="$UserpathOU",dc=POLIFORMADL.COM,dc=local";
+    New-ADUser -name $Username -GivenName $Givenname -SurName $Surname -DisplayName $Displayname -Path "ou="$UserpathOU",dc=POLIFORMADL,dc=COM";
     
-}ste
+}
+
+function Show-Header()
+{
+    Clear
+
+    Write-Host '      ____              __        '
+    Write-Host '     / __ \   ____ _   / /      ___ '
+    Write-Host '    / / / /  / __ `/  / /      / _ \'
+    Write-Host '   / /_/ /  / /_/ /  / /___   /  __/'
+    Write-Host '  /_____/   \__,_/  /_____/   \___/ '
+    Write-Host ''
+    Write-Host '    +-+-+-+-+-+-+-+-+-+-+ +-+-+-+'
+    Write-Host '    |P|o|w|e|r|s|h|e|l|l| |C|L|I|'
+    Write-Host '    +-+-+-+-+-+-+-+-+-+-+ +-+-+-+'
+    Write-Host ''
+    Write-Host '  >> Author : David Lejeune'
+    Write-Host "  >> Created : 27/09/2016"
+    Write-Host ''
+    Write-Host ' #####################################'
+    Write-Host ' #     ACTIVE DIRECTORY MANAGENT     #'
+    Write-Host ' #####################################'
+    Write-Host ''
+    Write-Host " Menu :";
+    Write-Host "";
+    Write-Host '    1. '$Menu1;
+    Write-Host '    2. '$Menu2;
+    Write-Host "";
+    
+}
+
+
+
+
+
+Show-Header;
+$Menu = Read-Host -Prompt 'Select an option ';
+
+switch ($Menu) 
+    { 
+        1 
+        {
+            Write-Host 'You have selected'$Menu1;
+            Create-OU;
+        } 
+        2 
+        {
+            Write-Host 'You have selected'$Menu2;
+            Create-User;
+        } 
+        default {"The choice could not be determined."}
+    }
+
