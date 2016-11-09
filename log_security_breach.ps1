eb3617d841ffdc2a754f01ef61b0e92f5fc96b14
David Lejeune <Lejeune.David@outlook.com>
2016-11-09 22:37:29 +0100

    adapted csv for seperate date and time stamp + converted to euro format

--- log_security_breach.ps1
+++ log_security_breach.ps1
@@ -1,3 +1,4 @@
-$Date = Get-Date
-$Entry = $Date.ToString() + "," + [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.ToString() + "," + $Descr.ToString() + ","
+$Date = (Get-Date).ToString('dd/MM/yyyy')
+$Time = (Get-Date).ToString('HH:mm:ss')
+$Entry = $Date.ToString() + "," + $Time.ToString() + "," + [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.ToString() + "," + $Descr.ToString() + ","
 Add-Content log_security_breach.csv $Entry
